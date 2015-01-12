a2f48c20cf7eda89cedbd205655018b6894a7136
hcarreras <haricarreras@gmail.com>
2015-01-12 18:59:09 +0100

    REST sin pedir credenciales

--- app/controllers/stock_controller.rb
+++ app/controllers/stock_controller.rb
@@ -1,61 +1,30 @@
 require 'google_drive/google_docs'
 class StockController < ApplicationController
-  before_filter :google_drive_login, :only => [:index, :show]
+  before_action :login
 
   def index
-    google_session = GoogleDrive.login_with_oauth(session[:google_token])
-    @sheet = google_session.spreadsheet_by_key("0AgWDbm7D_t2RdGFfdldFX3Z1aFllRG83bjZTYzU5VkE").worksheets[0]
+    @sheet = document
 
     respond_to do |format|
       format.html
-      format.json{ render json: @sheet.rows[2..-1].map{|row| row_to_hash(@sheet, row)}}
+      format.json{ render json: document.rows[2..-1].map{|row| row_to_hash(document, row)}}
     end
   end
 
   def show
-    google_session = GoogleDrive.login_with_oauth(session[:google_token])
-    @sheet = google_session.spreadsheet_by_key("0AgWDbm7D_t2RdGFfdldFX3Z1aFllRG83bjZTYzU5VkE").worksheets[0]
-    rows = @sheet.rows[1..-1]
+    rows = document.rows[1..-1]
 
     respond_to do |format|
-      format.json{ render json: row_to_hash(@sheet, rows[params[:id].to_i])}
+      format.json{ render json: row_to_hash(document, rows[params[:id].to_i])}
     end
   end
 
-  def list_google_docs
-    google_session = GoogleDrive.login_with_oauth(session[:google_token])
-    @google_docs = []
-    for file in google_session.files
-      @google_docs << file.title
-    end
+  def login
+    @session = GoogleDrive.login(ENV["GDRIVE_USERNAME"], ENV["GDRIVE_PASSWORD"])
   end
 
-  def download_google_docs
-    file_name = params[:doc_upload]
-    file_name_session = GoogleDrive.login_with_oauth(session[:google_token])
-    file_path = Rails.root.join('tmp', "doc_#{file_name_session}")
-    file = google_session.file_by_title(file_name)
-    file.download_to_file(file_path)
-    redirect_to list_google_doc_path
-  end
-
-  def set_google_drive_token
-    google_doc = GoogleDrive::GoogleDocs.new(ENV['google_client_id'], ENV['google_client_secret'],
-                                             ENV['google_client_redirect_uri'])
-    oauth_client = google_doc.create_google_client
-    auth_token = oauth_client.auth_code.get_token(params[:code],
-                                                  :redirect_uri => ENV['google_client_redirect_uri'])
-    session[:google_token] = auth_token.token if auth_token
-    redirect_to stock_index_path
-  end
-
-  def google_drive_login
-    unless session[:google_token].present?
-      google_drive = GoogleDrive::GoogleDocs.new(ENV['google_client_id'], ENV['google_client_secret'],
-                                                 ENV['google_client_redirect_uri'])
-      auth_url = google_drive.set_google_authorize_url
-      redirect_to auth_url
-    end
+  def document
+    @session.spreadsheet_by_key("0AgWDbm7D_t2RdGFfdldFX3Z1aFllRG83bjZTYzU5VkE").worksheets[0]
   end
 
   private
