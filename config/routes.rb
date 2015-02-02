a9e26fb04ef3948efa6ae4c30f4ccbae5b78eb75
hcarreras <haricarreras@gmail.com>
2015-02-02 23:27:37 +0100

    Routes default json

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 RailsApp::Application.routes.draw do
   root 'static_pages#index'
-  resources :stock, except: [:destroy]
-  namespace :stock do
+  resources :stock, except: [:destroy], defaults: {format: "json"}
+  namespace :stock, defaults: {format: "json"} do
     resource :search, only:[:create]
   end
 end
